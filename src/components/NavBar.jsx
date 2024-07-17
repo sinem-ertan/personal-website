a1e9d44b9a874f8f63791dd3c22c57045a3f6828
sinem-ertan <ssinemertan@gmail.com>
2024-07-18 00:21:50 +0300

    Fix nav items

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,12 +8,12 @@ function NavBar() {
       <div className="w-16 h-16 rounded-full bg-lightpurple text-darkpurple flex items-center justify-center text-3xl font-semibold rotate-45 dark:bg-darkpurple dark:text-darkmodelightpurple">
         S
       </div>
-      <nav className="flex w-3/12">
+      <nav className="flex">
         <ul className="flex w-full justify-between items-center text-gray-500 dark:text-darkmodegray">
-          <li className="cursor-pointer">
+          <li className="cursor-pointer mr-10">
             <a href="#skills">{t("skills")}</a>
           </li>
-          <li className="cursor-pointer">
+          <li className="cursor-pointer mr-10">
             <a href="#projects">{t("projects")}</a>
           </li>
           <li className="text-darkpurple border-darkpurple pl-8 pr-8 pt-3 pb-3 border rounded-lg h-max dark:border-darkmodepalepurple dark:bg-darkmodepalepurple cursor-pointer">
